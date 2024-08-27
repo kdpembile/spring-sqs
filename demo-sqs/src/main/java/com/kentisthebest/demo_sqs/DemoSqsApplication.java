19c19e866735ac81f02641c9891fa422cb6b1fa9
kente <kent.embile@gmail.com>
2024-08-27 21:52:58 +0800

    Initial commit

---
+++ demo-sqs/src/main/java/com/kentisthebest/demo_sqs/DemoSqsApplication.java
@@ -0,0 +1,13 @@
+package com.kentisthebest.demo_sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSqsApplication.class, args);
+	}
+
+}
